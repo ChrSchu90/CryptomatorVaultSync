999e2beba69ab23e45aa47a055225f92e6f06745
ChrSchu <chr.schu@outlook.com>
2026-06-01 20:04:35 +0200

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -11,7 +11,7 @@ RCLONE_RELEASE=1.74.2
 mkdir -p ./debug/sync ./debug/vault ./debug/rclone
 
 # Create Rclone config
-#docker run --rm -it -v ./debug/rclone:/rclone rclone/rclone config --config /rclone/rclone.conf
+# docker run --rm -it -v ./debug/config:/config rclone/rclone config --config /config/rclone.conf
 
 docker buildx build --load --progress=plain --platform ${DOCKER_PLATFORM} --build-arg CRYPTOMATOR_CLI_RELEASE=${CRYPTOMATOR_CLI_RELEASE} --build-arg RCLONE_RELEASE=${RCLONE_RELEASE} -f ${DOCKER_FILE} -t ${IMAGE_NAME} . && \
   docker run --rm -it --platform ${DOCKER_PLATFORM} \
